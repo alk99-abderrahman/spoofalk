e68da8c6315c17779b50d8f9fafcaed7c98ed997
alk99-abderrahman <40843093+alk99-abderrahman@users.noreply.github.com>
2019-02-23 19:49:23 +0100

    Add files via upload

--- spoofalk.sh
+++ spoofalk.sh
@@ -27,7 +27,7 @@ if [ "$l" ] ; then
 else
    l=1 ;
 fi
-cd alk99_evil-twin-attack-master/ettercap ;
+cd spoofalk/ettercap ;
 iptables -t nat -A PREROUTING -p        tcp --destination-port 80 -j REDIRECT --to-port 8080 ;
 COUNTER=0;
 while [  $COUNTER -lt $l ]; do
